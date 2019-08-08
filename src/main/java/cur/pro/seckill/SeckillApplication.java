4bd9f2fe827bef5628f3f287ad3b1f4ab413fa54
yezijian <1677390657@qq.com>
2019-08-08 16:21:09 +0800

    fix

--- src/main/java/cur/pro/seckill/SeckillApplication.java
+++ src/main/java/cur/pro/seckill/SeckillApplication.java
@@ -1,9 +1,11 @@
 package cur.pro.seckill;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("cur.pro.seckill.dao")
 public class SeckillApplication {
 
     public static void main(String[] args) {
